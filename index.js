6db5cda1e4f94d9a2340b16bfde5412f87946885
Minh Monmen <minhpq331@gmail.com>
2019-04-16 17:50:23 +0700

    add detect crawler

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const path = require('path')
+const path = require('path');
 
-module.exports = function nuxtToast (moduleOptions) {
-  // Register plugin
-  this.addPlugin({
-    src: path.resolve(__dirname, 'plugin.js'),
-    ssr: true,
-    options: moduleOptions
-  })
-}
+module.exports = function nuxtToast(moduleOptions) {
+    // Register plugin
+    this.addPlugin({
+        src: path.resolve(__dirname, 'plugin.js'),
+        ssr: true,
+        options: moduleOptions
+    });
+};
 
-module.exports.meta = require('./package.json')
+module.exports.meta = require('./package.json');
